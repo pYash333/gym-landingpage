6743aaa7704d5c3ed62811b9fffaee153d8cc91b
yash <yporwal333@gmail.com>
2024-11-26 22:33:33 +0530

    membership and about us section created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,24 @@
-$(function(){
-    
-})
+$(function () {
+  if ($(".about-us").length > 0) {
+    $(".about-us .aboutus-slider").slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: true,
+      centerMode: false,
+      focusOnSelect: true,
+      infinite: true,
+      loop: true,
+      responsive: [
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: false,
+            arrows: false
+          },
+        },
+      ],
+    });
+  }
+});
