f87840691448e8319bb165f1da8d4a4adfc9d05d
yash <yporwal333@gmail.com>
2024-11-27 07:35:05 +0530

    staff section created

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,10 +15,51 @@ $(function () {
             slidesToShow: 1,
             slidesToScroll: 1,
             dots: false,
-            arrows: false
+            arrows: false,
           },
         },
       ],
     });
+  }
+
+  if (window.matchMedia("(max-width: 767px)").matches) {
+    if ($(".membership-section").length > 0) {
+      $(".membership-section .membership-slider").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: false,
+        centerMode: false,
+        focusOnSelect: true,
+        infinite: true,
+        loop: true,
+        // adaptiveHeight: true
+      });
+    }
+  }
+  if (window.matchMedia("(max-width: 991px)").matches) {
+    if ($(".staff-section").length > 0) {
+      $(".staff-section .staff-slider").slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: false,
+        centerMode: false,
+        focusOnSelect: true,
+        infinite: true,
+        loop: true,
+        responsive: [
+          {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              dots: false,
+              arrows: false,
+            },
+          },
+        ],
+      });
+    }
   }
 });
